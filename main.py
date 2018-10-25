2b842d219abcf3051e16f533a4df011db59c6d67
AeroXi <31943024+AeroXi@users.noreply.github.com>
2018-10-25 23:41:45 +0800

    add position filter and servor angle limit

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from control import *
 
 def main():
     p = float(input('p = '))
+    angle_threshold = int(input('angle threshold = '))
     cap = cv2.VideoCapture(0)
-    last_position = 0
+    last_angle = 0
     while True:
         ret, frame = cap.read()
         result, position = process(frame)
-        if position == -100:
-            angle = p_control(last_position, p)
-        else:
-            angle = p_control(position, p)
-        print(f'angle = {angle}')
+        if position != -1000:
+            angle = get_angle(position, p)
+            if abs(angle - last_angle) < angle_threshold:
+                last_angle = angle
+                print(f'angle = {angle}')
         cv2.namedWindow("capture", cv2.WINDOW_NORMAL)
         cv2.imshow("capture", result)
         if cv2.waitKey(1) & 0xFF == ord('q'):
